4d128f941136978286abc54eae29a730b4c41353
DaleFu22 <116727779+DaleFu22@users.noreply.github.com>
2023-11-07 17:25:44 +0800

    Add files via upload

---
+++ C_language/12_day/multifile/include/caculat.h
@@ -0,0 +1,12 @@
+#ifndef __CACULAT_H
+#define __CACULAT_H
+
+#include <stdio.h>
+#include "fun.h"
+
+int add_num(int a,int b);
+int sub_num(int a,int b);
+int mul_num(int a,int b);
+int div_num(int a,int b);
+
+#endif
---
+++ C_language/12_day/multifile/include/fun.h
@@ -0,0 +1,13 @@
+#ifndef __FUN_H
+#define __FUN_H
+
+#include <stdio.h>
+
+struct stu{
+	int age;
+	char name[50];
+	float score;
+};
+
+void Print_Struct(void);
+#endif
